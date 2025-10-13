896dda62293fedb5a2c6673cbd14d1d1a4e294c5
baohuy6505 <baohuy6505@gmail.com>
2025-10-13 20:28:28 +0700

    remove file

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const usersRouter = require('./users');
-    // app.user('/users', usersRouter);
-    router.use('/users', usersRouter);
-
-module.exports = router;
